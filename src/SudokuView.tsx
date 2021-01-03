12ab55be6d5b76f70451d86279268ebbcd82d561
Anoop Naravaram <anoop901@gmail.com>
2021-01-02 23:45:37 -0600

    In SudokuView, display possible values in each cell

--- src/SudokuView.tsx
+++ src/SudokuView.tsx
@@ -17,62 +17,61 @@ const selectedCellStyle = {
   backgroundColor: "#bbf",
 };
 
+const possibleValuesStyle = {
+  fontSize: "0.7em",
+  color: "#888",
+};
+
 export default function SudokuView(props: { sudoku: Sudoku }) {
   const [selectedLocation, setSelectedLocation] = useState(
     new SudokuLocation({ row: 0, col: 0 })
   );
-  const possibleValuesAtSelectedLocation = props.sudoku.possibleValuesAtLocation(
-    selectedLocation
-  );
   return (
-    <>
-      <table>
-        {Array.from({ length: 3 }, (_, boxRow) => (
-          <tr>
-            {Array.from({ length: 3 }, (_, boxCol) => (
-              <td>
-                <table>
-                  {Array.from({ length: 3 }, (_, rowInBox) => (
-                    <tr>
-                      {Array.from({ length: 3 }, (_, colInBox) => {
-                        const row = boxRow * 3 + rowInBox;
-                        const col = boxCol * 3 + colInBox;
-                        const location = new SudokuLocation({ row, col });
-                        const isSelectedCell = selectedLocation.equals(
-                          location
-                        );
-                        const isAdjacentToSelectedCell = selectedLocation.adjacentLocations.has(
-                          location
-                        );
-                        return (
-                          <td
-                            style={{
-                              ...tableCellStyle,
-                              ...(isSelectedCell ? selectedCellStyle : {}),
-                              ...(isAdjacentToSelectedCell
-                                ? adjacentCellStyle
-                                : {}),
-                            }}
-                            onClick={() => {
-                              setSelectedLocation(location);
-                            }}
-                          >
-                            {props.sudoku.valueAtLocation(location) ?? ""}
-                          </td>
-                        );
-                      })}
-                    </tr>
-                  ))}
-                </table>
-              </td>
-            ))}
-          </tr>
-        ))}
-      </table>
-      <p>
-        The currently selected cell could be any of: [
-        {possibleValuesAtSelectedLocation.join(", ")}]
-      </p>
-    </>
+    <table>
+      {Array.from({ length: 3 }, (_, boxRow) => (
+        <tr>
+          {Array.from({ length: 3 }, (_, boxCol) => (
+            <td>
+              <table>
+                {Array.from({ length: 3 }, (_, rowInBox) => (
+                  <tr>
+                    {Array.from({ length: 3 }, (_, colInBox) => {
+                      const row = boxRow * 3 + rowInBox;
+                      const col = boxCol * 3 + colInBox;
+                      const location = new SudokuLocation({ row, col });
+                      const isSelectedCell = selectedLocation.equals(location);
+                      const isAdjacentToSelectedCell = selectedLocation.adjacentLocations.has(
+                        location
+                      );
+                      const value = props.sudoku.valueAtLocation(location);
+                      return (
+                        <td
+                          style={{
+                            ...tableCellStyle,
+                            ...(isSelectedCell ? selectedCellStyle : {}),
+                            ...(isAdjacentToSelectedCell
+                              ? adjacentCellStyle
+                              : {}),
+                            ...(value === null ? possibleValuesStyle : {}),
+                          }}
+                          onClick={() => {
+                            setSelectedLocation(location);
+                          }}
+                        >
+                          {value ??
+                            props.sudoku
+                              .possibleValuesAtLocation(location)
+                              .join(" ")}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                ))}
+              </table>
+            </td>
+          ))}
+        </tr>
+      ))}
+    </table>
   );
 }
