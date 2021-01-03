4e39f4d67cbc9932c973435c4349db5489c7f897
Anoop Naravaram <anoop901@gmail.com>
2021-01-02 21:48:45 -0600

    Make Sudoku use immutable Lists

--- src/Sudoku.test.ts
+++ src/Sudoku.test.ts
@@ -1,63 +1,66 @@
+import { List } from "immutable";
 import Sudoku from "./Sudoku";
 
 describe("constructor", () => {
   test("with correct values size", () => {
     expect(
       () =>
-        new Sudoku([
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        ])
+        new Sudoku(
+          List([
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+          ])
+        )
     ).not.toThrowError(
       "failed to construct Sudoku: values must be a 9x9 array"
     );
   });
   test("with bad values size", () => {
     expect(
-      () =>
-        new Sudoku([
-          [1, 2, 3],
-          [1, 2, 3],
-        ])
+      () => new Sudoku(List([List([1, 2, 3]), List([1, 2, 3])]))
     ).toThrowError("failed to construct Sudoku: values must be a 9x9 array");
   });
   test("with bad values size 2", () => {
     expect(
       () =>
-        new Sudoku([
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-        ])
+        new Sudoku(
+          List([
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+          ])
+        )
     ).toThrowError("failed to construct Sudoku: values must be a 9x9 array");
   });
   test("with bad values size 3", () => {
     expect(
       () =>
-        new Sudoku([
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-          [1, 2, 3],
-        ])
+        new Sudoku(
+          List([
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+            List([1, 2, 3]),
+          ])
+        )
     ).toThrowError("failed to construct Sudoku: values must be a 9x9 array");
   });
 });
--- src/Sudoku.ts
+++ src/Sudoku.ts
@@ -1,10 +1,12 @@
+import { List } from "immutable";
+
 export default class Sudoku {
-  constructor(public readonly values: (number | null)[][]) {
-    if (values.length !== 9) {
+  constructor(public readonly values: List<List<number | null>>) {
+    if (values.size !== 9) {
       throw new Error("failed to construct Sudoku: values must be a 9x9 array");
     }
     for (const valuesRow of values) {
-      if (valuesRow.length !== 9) {
+      if (valuesRow.size !== 9) {
         throw new Error(
           "failed to construct Sudoku: values must be a 9x9 array"
         );
